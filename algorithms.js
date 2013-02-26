d3b7dc01104859d1b6c0977d216b8d2fbb807b3c
Conrad Benham <conrad@agilehongkong.com>
2013-02-26 00:11:26 -0500

    Replaced recursion of the siftDown method with iteration.

--- algorithms.js
+++ algorithms.js
@@ -1,8 +1,8 @@
 var ajs = {};
 
-ajs.AscendingNumericComparator = function () {
+ajs.AscendingNumericComparator = function() {
 };
-ajs.AscendingNumericComparator.prototype.compare = function (left, right) {
+ajs.AscendingNumericComparator.prototype.compare = function(left, right) {
     if (left < right) {
         return -1;
     } else if (left > right) {
@@ -12,40 +12,40 @@ ajs.AscendingNumericComparator.prototype.compare = function (left, right) {
     }
 };
 
-ajs.NegationComparator = function (delegateComparator) {
+ajs.NegationComparator = function(delegateComparator) {
     this.delegateComparator = delegateComparator;
 };
-ajs.NegationComparator.prototype.compare = function (left, right) {
+ajs.NegationComparator.prototype.compare = function(left, right) {
     return this.delegateComparator.compare(left, right) * -1;
 };
 
-ajs.DescendingNumericComparator = function () {
+ajs.DescendingNumericComparator = function() {
     this.delegateComparator = new ajs.NegationComparator(new ajs.AscendingNumericComparator());
 };
-ajs.DescendingNumericComparator.prototype.compare = function (left, right) {
+ajs.DescendingNumericComparator.prototype.compare = function(left, right) {
     return this.delegateComparator.compare(left, right);
 };
 
-ajs.Edge = function (predecessor, successor, weight) {
+ajs.Edge = function(predecessor, successor, weight) {
     this.predecessor = predecessor;
     this.successor = successor;
     this.weight = weight;
 };
-ajs.Edge.prototype.hasSuccessorForData = function (targetData) {
+ajs.Edge.prototype.hasSuccessorForData = function(targetData) {
     return this.successor.data === targetData;
 };
 
-ajs.Node = function (data) {
+ajs.Node = function(data) {
     this.data = data;
     this.adjacencies = [];
 };
-ajs.Node.prototype.connectTo = function (successorNode, weight) {
+ajs.Node.prototype.connectTo = function(successorNode, weight) {
     var edge = new ajs.Edge(this, successorNode, weight);
     this.adjacencies.push(edge);
 };
-ajs.Node.prototype.findEdgeTo = function (targetSuccessorData) {
+ajs.Node.prototype.findEdgeTo = function(targetSuccessorData) {
     var foundEdge = null;
-    this.adjacencies.forEach(function (edge) {
+    this.adjacencies.forEach(function(edge) {
         if (edge.hasSuccessorForData(targetSuccessorData)) {
             foundEdge = edge;
         }
@@ -53,7 +53,7 @@ ajs.Node.prototype.findEdgeTo = function (targetSuccessorData) {
     return foundEdge;
 };
 
-ajs.PriorityQueue = function () {
+ajs.PriorityQueue = function() {
     this.queue = [];
     var items = [];
     this.comparator = new ajs.AscendingNumericComparator();
@@ -71,27 +71,27 @@ ajs.PriorityQueue = function () {
     this.addAll(items);
 };
 
-ajs.PriorityQueue.prototype.peek = function () {
+ajs.PriorityQueue.prototype.peek = function() {
     if (this.isEmpty()) {
         return null;
     }
     return this.queue[0];
 };
 
-ajs.PriorityQueue.prototype.size = function () {
+ajs.PriorityQueue.prototype.size = function() {
     return this.queue.length;
 };
 
-ajs.PriorityQueue.prototype.isEmpty = function () {
+ajs.PriorityQueue.prototype.isEmpty = function() {
     return this.queue.length == 0;
 };
 
-ajs.PriorityQueue.prototype.add = function (item) {
+ajs.PriorityQueue.prototype.add = function(item) {
     this.queue.push(item);
     this.siftUp(this.queue.length);
 };
 
-ajs.PriorityQueue.prototype.addAll = function () {
+ajs.PriorityQueue.prototype.addAll = function() {
     var items = arguments;
     if (arguments.length == 1 && arguments[0] instanceof Array) {
         items = arguments[0];
@@ -101,7 +101,7 @@ ajs.PriorityQueue.prototype.addAll = function () {
     }
 };
 
-ajs.PriorityQueue.prototype.siftUp = function (initialIndex) {
+ajs.PriorityQueue.prototype.siftUp = function(initialIndex) {
     var currentIndex = initialIndex;
     var topReached = false;
     while(currentIndex > 1 && !topReached) {
@@ -119,7 +119,7 @@ ajs.PriorityQueue.prototype.siftUp = function (initialIndex) {
     }
 };
 
-ajs.PriorityQueue.prototype.poll = function () {
+ajs.PriorityQueue.prototype.poll = function() {
     if (this.isEmpty()) {
         return null;
     }
@@ -132,26 +132,35 @@ ajs.PriorityQueue.prototype.poll = function () {
     return valueToReturn;
 };
 
-ajs.PriorityQueue.prototype.siftDown = function (currentIndex) {
+ajs.PriorityQueue.prototype.siftDown = function(initialIndex) {
     var length = this.queue.length;
-    var indexOfLeftChild = (currentIndex * 2) + 1;
-    var indexOfRightChild = indexOfLeftChild + 1;
-    var minimumIndex;
+    var currentIndex = initialIndex;
 
-    if (indexOfRightChild > length) {
-        return;
-    } else if (indexOfRightChild < length) {
-        var left = this.queue[indexOfLeftChild];
-        var right = this.queue[indexOfRightChild];
-        minimumIndex = this.comparator.compare(left, right) < 0 ? indexOfLeftChild : indexOfRightChild;
-    } else {
-        minimumIndex = indexOfLeftChild;
-    }
+    var atEnd = false;
+    while(!atEnd) {
+        var indexOfLeftChild = (currentIndex * 2) + 1;
+        var indexOfRightChild = indexOfLeftChild + 1;
+        var minimumIndex;
 
-    if (this.comparator.compare(this.queue[minimumIndex], this.queue[currentIndex]) < 0) {
-        var temp = this.queue[minimumIndex];
-        this.queue[minimumIndex] = this.queue[currentIndex];
-        this.queue[currentIndex] = temp;
-        this.siftDown(minimumIndex);
+        if (indexOfRightChild <= length) {
+            if (indexOfRightChild < length) {
+                var left = this.queue[indexOfLeftChild];
+                var right = this.queue[indexOfRightChild];
+                minimumIndex = this.comparator.compare(left, right) < 0 ? indexOfLeftChild : indexOfRightChild;
+            } else {
+                minimumIndex = indexOfLeftChild;
+            }
+
+            if (this.comparator.compare(this.queue[minimumIndex], this.queue[currentIndex]) < 0) {
+                var temp = this.queue[minimumIndex];
+                this.queue[minimumIndex] = this.queue[currentIndex];
+                this.queue[currentIndex] = temp;
+                currentIndex = minimumIndex;
+            } else {
+                atEnd = true;
+            }
+        } else {
+            atEnd = true;
+        }
     }
 };
